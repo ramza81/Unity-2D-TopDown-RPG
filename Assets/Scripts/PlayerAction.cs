2c3431d0041b6bdc6321fcd175cd53af1cb87723
unknown <cloudbridge@kolon.com>
2020-11-06 17:09:49 +0900

    'B20'

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    float h;
+    float v;
+
+    Rigidbody2D rigid;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() {
+        rigid.velocity = new Vector2(h, v);
+    }
+    
+}
