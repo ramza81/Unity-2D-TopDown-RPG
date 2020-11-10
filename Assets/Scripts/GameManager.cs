2066b72de1acd60aaf65a46591f32f4fbb54ad82
unknown <cloudbridge@kolon.com>
2020-11-10 16:38:41 +0900

    'B22'

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject talkPanel;
+    public Text talkText;
+    public GameObject scanObject;
+    public bool isAction;
+
+    public void Action(GameObject scanObj)
+    {
+        if (isAction)
+        {
+            isAction = false;
+        }
+        else
+        {
+            isAction = true;
+            scanObject = scanObj;
+            talkText.text = $"이것의 이름은 {scanObject.name} 이라고 한다.";
+        }
+
+        talkPanel.SetActive(isAction);
+    }
+}
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAction : MonoBehaviour
 {
     public float Speed;
+    public GameManager manager;
     float h;
     float v;
     bool isHorizonMove;
@@ -23,14 +24,14 @@ public class PlayerAction : MonoBehaviour
     private void Update() 
     {
         // Move Value
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        h = manager.isAction ? 0 : Input.GetAxisRaw("Horizontal");
+        v = manager.isAction ? 0 : Input.GetAxisRaw("Vertical");
 
         // Check Button Down & Up
-        bool hDown = Input.GetButtonDown("Horizontal");
-        bool vDown = Input.GetButtonDown("Vertical");
-        bool hUp = Input.GetButtonUp("Horizontal");
-        bool vUp = Input.GetButtonUp("Vertical");
+        bool hDown = manager.isAction ? false : Input.GetButtonDown("Horizontal");
+        bool vDown = manager.isAction ? false : Input.GetButtonDown("Vertical");
+        bool hUp = manager.isAction ? false : Input.GetButtonUp("Horizontal");
+        bool vUp = manager.isAction ? false : Input.GetButtonUp("Vertical");
 
         // Check Horizonatal Move
         if (hDown)
@@ -83,7 +84,8 @@ public class PlayerAction : MonoBehaviour
         // Scan Object
         if (Input.GetButtonDown("Jump") && scanObject != null)
         {
-            Debug.Log($"This is {scanObject.name}");
+            // Debug.Log($"This is {scanObject.name}");
+            manager.Action(scanObject);
         }
     }
 
