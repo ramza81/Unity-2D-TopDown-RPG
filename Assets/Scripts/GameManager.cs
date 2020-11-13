8e638f9f66dbe1cb4edcd5e604edfb548364cdba
unknown <cloudbridge@kolon.com>
2020-11-13 15:17:49 +0900

    'B25'

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject talkPanel;
-    public Text talkText;
+    public TalkManager talkManager;
+    public QuestManager questManager;
+    public Animator talkPanel;
+    public Animator portraitAnim;
+    public Image portraitImg;
+    public TypeEffect talk;
     public GameObject scanObject;
     public bool isAction;
-    public TalkManager talkManager;
     public int talkIndex;
-    public Image portraitImg;
-    public QuestManager questManager;
+    public Sprite prevPortrait;
 
     void Start() 
     {
@@ -27,14 +29,25 @@ public class GameManager : MonoBehaviour
         Talk(objectData.id, objectData.isNpc);
 
         // Visible Talk for Action
-        talkPanel.SetActive(isAction);
+        talkPanel.SetBool("isShow", isAction);
     }
 
     void Talk(int id, bool isNpc)
     {
         // Set Talk Data
-        int questTalkIndex = questManager.GetQuestTalkIndex(id);
-        string talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        int questTalkIndex = 0;
+        string talkData = "";
+
+        if (talk.isAni)
+        {
+            talk.SetMsg("");
+            return;
+        }
+        else
+        {
+            questTalkIndex = questManager.GetQuestTalkIndex(id);
+            talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        }
 
         // End Talk
         if (talkData == null)
@@ -48,15 +61,23 @@ public class GameManager : MonoBehaviour
         // Continue Talk
         if (isNpc)
         {
-            talkText.text = talkData.Split(':')[0];
+            talk.SetMsg(talkData.Split(':')[0]);
 
             // Show Portrait
             portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
             portraitImg.color = new Color(1, 1, 1, 1);
+
+            // Animation Portrait
+            if (prevPortrait != portraitImg.sprite)
+            {
+                portraitAnim.SetTrigger("doEffect");
+                prevPortrait = portraitImg.sprite;
+            }
+            
         }
         else
         {
-            talkText.text = talkData;
+            talk.SetMsg(talkData);
             portraitImg.color = new Color(1, 1, 1, 0);
         }
 
---
+++ Assets/Scripts/TypeEffect.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeEffect : MonoBehaviour
+{
+    public int CharPerSeconds;
+    public GameObject EndCursor;
+    public bool isAni;
+    
+    Text msgText;
+    AudioSource audioSource;
+
+    string targetMsg;
+    int index;
+    float interval;
+
+
+    private void Awake() 
+    {
+        msgText = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SetMsg(string msg)
+    {
+        if (isAni)
+        {
+            msgText.text = targetMsg;
+            CancelInvoke();
+            EffectEnd();
+        }
+        else
+        {
+            targetMsg = msg;
+            EffectStart();
+        }
+    }
+
+    void EffectStart()
+    {
+        msgText.text = "";
+        index = 0;
+        EndCursor.SetActive(false);
+
+        // Start Animation
+        interval = 1.0f / CharPerSeconds;        
+
+        isAni = true;
+        Invoke("Effecting", 1 / CharPerSeconds);
+    }
+
+    void Effecting()
+    {
+        // End Animation
+        if (msgText.text == targetMsg)
+        {
+            EffectEnd();
+            return;
+        }
+
+        msgText.text += targetMsg[index];
+
+        // Sound
+        if (targetMsg[index] != ' ' || targetMsg[index] != '.')
+        {
+            audioSource.Play();            
+        }     
+        
+        index++;
+
+        // Recursive
+        Invoke("Effecting", interval);
+    }    
+
+    void EffectEnd()
+    {
+        isAni = false;
+        EndCursor.SetActive(true);
+    }      
+}
